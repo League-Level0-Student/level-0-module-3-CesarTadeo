cbde22c65e246ee6788ef7413f90cffbf26bde3a
eliyahubowers <league@WTS-IM-6.attlocal.net>
2019-03-31 12:43:45 -0700

    learning the % thing

--- src/loops/WalkOfFame.java
+++ src/loops/WalkOfFame.java
@@ -11,19 +11,28 @@ public class WalkOfFame {
 		Robot rob = new Robot();
 		// 1. Set the X position of the robot so that it starts on the left. 
 		// You also need to show the robot to see the result of this line.
+		rob.setX(50);
+		rob.setSpeed(500);
+		// 2.
+		//Make the robot draw a star shape. Hint: 144.
+		for (int i=0;i<=5;i++){ 
+		rob.penDown();
+		rob.move(30);
+		rob.turn(144);
+		}
+		// Set the size of the star to 3O.
 
-		// 2. Make the robot draw a star shape. Hint: 144.
-
-		// 3. Set the size of the star to 30.
-
+		
 		/** THE CHALLENGE: **/
 		/*
-		 * Make the robot draw a line of stars like this:
+		 * Makes the robot draw a line of stars like this:
 		 * http://bit.ly/RobotWalk
 		 * 
 		 * Hint: The distance between stars is 50.
 		 */
 
+		
+		
 	}
 
 }
