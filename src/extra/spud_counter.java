1f0747d17c1309a6cde935ef396051dab216e448
League Student <league@iMac-21.attlocal.net>
2019-07-14 12:04:49 -0700

    finished module 3, spud counter

---
+++ src/extra/spud_counter.java
@@ -0,0 +1,28 @@
+package extra;
+
+public class spud_counter {
+
+public static void main(String[] args) {
+	
+	for (int i = 1; i < 9; i++) {
+		
+		
+		if (i == 4) {
+			System.out.println("4");
+		}
+		else if  (i==8) {
+			System.out.println("more");
+		}
+		else {
+			System.out.println(i + "potato");
+		}
+	
+			
+			
+			
+			
+			
+	}
+	}
+}	
+
